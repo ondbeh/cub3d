0845e743a836d378e05894b7175f636378ebb251
Konrad Muhlbauer <75761278+Komu211@users.noreply.github.com>
2025-02-19 18:01:44 +0100

    removed redudant thing

--- src/init/init.c
+++ src/init/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: obehavka <obehavka@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 23:02:57 by obehavka          #+#    #+#             */
-/*   Updated: 2025/02/19 17:20:58 by obehavka         ###   ########.fr       */
+/*   Updated: 2025/02/19 17:59:00 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,8 +58,6 @@ void	cub3d_init(t_cub3d *cub3d)
 
 	textures = (t_textures){NULL, NULL, NULL, NULL};
 	cub3d->map_lines = NULL;
-	cub3d->ceiling_color = 0x87CEEBFF;
-	cub3d->floor_color = 0x8B4513FF;
 	set_default_values(cub3d);
 	parse_file(cub3d, &textures, "map.cub");
 	load_textures(cub3d, textures);
